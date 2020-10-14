4030a1d311858deb45edeafd26f06efb9603aa80
ankitanwar <tanwariam@gmail.com>
2020-10-14 22:54:11 +0530

    added simple file

--- buffer/main.go
+++ buffer/main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	do_simple()
+}
+
+func do_simple() {
+	sm := example_simple.Person{
+		id:         12345,
+		first_name: "first",
+		last_name:  "last",
+
+		is_verified: true,
+		heigh:       178.5,
+	}
+
+	fmt.Println(sm)
 }
