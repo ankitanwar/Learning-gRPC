de3721e9a0a8e14d2c4c4be5b78456e9767d16be
ankitanwar <tanwariam@gmail.com>
2020-10-15 00:00:49 +0530

    modified

--- buffer/main.go
+++ buffer/main.go
@@ -3,18 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world")
-	do_simple()
+	fmt.Println("First Protocol Buffer")
+	doSimple()
 }
 
-func do_simple() {
-	sm := example_simple.Person{
-		id:         12345,
-		first_name: "first",
-		last_name:  "last",
-
+func doSimple() {
+	sm := right.Person{
+		age:         22,
+		first_name:  "first",
+		last_name:   "last",
 		is_verified: true,
-		heigh:       178.5,
 	}
 
 	fmt.Println(sm)
